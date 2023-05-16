fd4fc0a7c29ecfe8ef372cad7b8fc47797107be0
Harshdeep Singh <harshdeeps1313@gmail.com>
2023-05-16 18:54:06 +0530

    Added Integration controller and dependencies resolve

---
+++ CyzergFullStackTechnicalAssessment/API/Cyzerg.TA.API/Controllers/IntegrationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Cyzerg.TA.BusinessService;
+
+namespace Cyzerg.TA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+
+    }
+}
---
+++ CyzergFullStackTechnicalAssessment/API/Cyzerg.TA.BusinessService/Integration/IntegrationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyzerg.TA.BusinessService.Integration
+{
+    internal class IntegrationService : IIntegrationService
+    {
+    }
+}
